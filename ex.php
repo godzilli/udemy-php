268e2e185d29ced0894a11ad5f3a135ed7a44b34
Jennie <jenniferdacostazilli@hotmail.com>
2021-08-21 22:19:59 -0300

    phpnav

--- ex.php
+++ ex.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/ex.css">
     <title>Exercicio</title>
 </head>
-<body>
-    
+
+<body class="ex">
+
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Atividade</h2>
     </header>
 
-    <main class="principal">
-        <div class="conteudo">
+    <nav class=navegacao>
+        <a href="index.php" class=vermelho>Voltar</a>
+    </nav>
 
-        </div>
+    <main class="principal">
+        <div class="conteudo"></div>
     </main>
 
     <footer class="rodape">
--- index.php
+++ index.php
@@ -1,82 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Local de estudo PHP</title>
-</head>
-<body>
-    
-    <header class="cabecalho">
-        <h1>Curso PHP</h1>
-        <h2>Início</h2>
-    </header>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Local de estudo PHP</title>
+    </head>
+    <body>
+        
+        <header class="cabecalho">
+            <h1>Curso PHP</h1>
+            <h2>Início</h2>
+        </header>
 
-    <main class="principal">
-        <div class="conteudo">
-            <nav class="modulos"> <!-- navegação-->
-                <div class="modulo verde"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 01</h3>
-                        <ul>
-                            <li><a href="ex.php">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                            
-                        </ul>
-                </div>
+        <main class="principal">
+            <div class="conteudo">
+                <nav class="modulos"> <!-- navegação-->
+                    <div class="modulo verde"> <!--link que redireciona para a atividade-->
+                        <h3>Módulos 01</h3>
+                            <ul>
+                                <li><a href="ex.php">Atividade A</a></li><!--links-->
+                                <li><a href="#">Atividade B</a></li>
+                                <li><a href="#">Atividade C</a></li>
+                                
+                            </ul>
+                    </div>
 
-                <div class="modulo vermelho"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 02</h3>
-                        <ul>
-                            <li><a href="#">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                        </ul>
-                </div>
-                <div class="modulo azul"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 03</h3>
-                        <ul>
-                            <li><a href="#">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                        </ul>
-                </div>
-                <div class="modulo laranja"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 04</h3>
-                        <ul>
-                            <li><a href="#">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                        </ul>
-                </div>
-                <div class="modulo verde"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 05</h3>
-                        <ul>
-                            <li><a href="#">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                        </ul>
-                </div>
-                <div class="modulo vermelho"> <!--link que redireciona para a atividade-->
-                    <h3>Módulos 06</h3>
-                        <ul>
-                            <li><a href="#">Atividade A</a></li><!--links-->
-                            <li><a href="#">Atividade B</a></li>
-                            <li><a href="#">Atividade C</a></li>
-                        </ul>
-                </div>
-            </nav>
+                    <div class="modulo vermelho"> <!--link que redireciona para a atividade-->
+                        <h3>Módulos 02</h3>
+                            <ul>
+                                <li><a href="#">Atividade A</a></li><!--links-->
+                                <li><a href="#">atividade B</a></li>
+                                <li><a href="#">atividade C</a></li>
+                            </ul>
+                    </div>
+                </nav>
+            </div>
+        </main>
 
-        </div>
-    </main>
+        <footer class="rodape">
+            Jennifer Zilli - jenniferdacostazilli@hotmail.com <?= date('M-D-Y') ?>
+        </footer>
 
-    <footer class="rodape">
-        Jennifer Zilli - jenniferdacostazilli@hotmail.com <?= date('M-D-Y') ?>
-    </footer>
-
- <!--
-     comentario
- -->
-</body>
-</html>
+    <!--
+        comentario
+    -->
+    </body>
+    </html>
